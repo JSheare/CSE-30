b098856f747ea088058be00ac6d0de713f4fc77b
JSheare <jrsheare@ucsc.edu>
2023-04-19 13:53:07 -0700

    update

--- Quiz 1/point2d.py
+++ Quiz 1/point2d.py
@@ -3,8 +3,8 @@ import math as math
 
 class Point2D:
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        self.x = float(x)
+        self.y = float(y)
 
     def __str__(self):
         return f'({self.x}, {self.y})'
