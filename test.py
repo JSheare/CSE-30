8c9ee3e39446a8260fa6c2072088a1723162fd7c
JSheare <jrsheare@ucsc.edu>
2023-05-30 10:26:58 -0700

    update

--- test.py
+++ test.py
@@ -1,27 +1,47 @@
-import turtle
+from graph import Graph
+g = Graph()
+for i in range(9):
+    g.addVertex(i+1)
+
+g.addEdge(1, 3)
+g.addEdge(1, 4)
+
+g.addEdge(2, 3)
+g.addEdge(2, 5)
+g.addEdge(2, 9)
+
+g.addEdge(3, 1)
+g.addEdge(3, 2)
+g.addEdge(3, 9)
+
+g.addEdge(4, 1)
+g.addEdge(4, 5)
+g.addEdge(4, 7)
+g.addEdge(4, 9)
+
+g.addEdge(5, 2)
+g.addEdge(5, 4)
+g.addEdge(5, 7)
+g.addEdge(5, 9)
+
+g.addEdge(6, 8)
+
+g.addEdge(7, 4)
+g.addEdge(7, 5)
+g.addEdge(7, 8)
+
+g.addEdge(8, 6)
+g.addEdge(8, 7)
+
+g.addEdge(9, 2)
+g.addEdge(9, 3)
+g.addEdge(9, 4)
+g.addEdge(9, 5)
+
+print(g.DFS(2, []))
+print(g.breadth_first_search(2))
 
 
-def draw_dragon(t, length, level, right=1):
-    t.pendown()
-    if level > 0:
-        t.forward(length)
-        t.left(90)
-        t.forward(length)
-        t.left(90)
-        for i in range(right):
-            t.forward(length)
-            t.right(90)
 
-        draw_dragon(t, length, level-1, right+1)
-
-
-if __name__ == '__main__':
-    s = turtle.Screen()
-    s.setup(450, 450)
-    t = turtle.Turtle()
-    t.pen(pencolor='black', pensize=2, speed=0)
-    for i in range(4):
-        t.forward(100)
-        t.left(90)
 
 
